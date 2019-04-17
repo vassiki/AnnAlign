4cdfb4b40f336a84a863246819e5e48a270eebea
vassiki <chauhan.vassiki@gmail.com>
2019-04-17 19:03:44 -0400

    fixed index error issue across all movie parts

--- annalign/oneD.py
+++ annalign/oneD.py
@@ -13,7 +13,7 @@ def format_file(fn):
 
 def contiguous_blocks(df, index):
     begins_with = index
-    while df.loc[index, 'End'] == df.loc[index+1, 'Start']:
+    while df.loc[index, 'End'] == df.loc[index+1, 'Start'] and index < df.shape[0] - 2:
         index += 1
     return begins_with, index
 
@@ -23,12 +23,11 @@ def timing1D(df):
     newdf = pd.DataFrame(columns=['start', 'end'])
     # start with index 0
     index = 0
-    while index < df.shape[0] -2:
+    while index < df.shape[0] -1:
         start, end = contiguous_blocks(df, index)
         row = pd.DataFrame([[df.loc[start, 'Start'], df.loc[end, 'End']]], columns=['start', 'end'])
         newdf = newdf.append(row)
         index = end+1
-
     return newdf
 
 
@@ -39,24 +38,15 @@ def timing_file(df, part, dimension):
     dimension is the one we want to extract time stamps for
     """
 
-    #df[dimension] = df[dimension].str.lower()
-    #df['Start'] = np.round(df['Start'], 1)
-    #df = df.drop_duplicates(['Start'])
-    # df = df.sort_values(by=['Start'])
-    # not_present_df = df[df[dimension].isnull()]
-    # present_df = df[~df['Face Present'].isnull()]
     newdf = timing1D(df)
     prefix = '_'.join(['part_{0}'.format(str(part))] +
                       [val.lower() for val in dimension.split(' ')])
 
     start_times_present = newdf['start'].values
-    print(type(start_times_present))
-    #start_times_present = present_df["Start"].values
     np.savetxt(os.path.join("../outputs","{0}_annot_present.txt".format(prefix)), start_times_present.reshape(1, start_times_present.shape[0]),
                fmt='%4.1f')
 
     start_times_absent = newdf['end'].values
-    #start_times_absent = not_present_df["Start"].values
     np.savetxt(os.path.join("../outputs","{0}_annot_absent.txt".format(prefix)), start_times_absent.reshape(1, start_times_absent.shape[0]),
                fmt='%4.1f')
     return start_times_present, start_times_absent
